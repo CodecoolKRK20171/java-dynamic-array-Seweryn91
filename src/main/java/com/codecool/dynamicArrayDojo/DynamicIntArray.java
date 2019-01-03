467f8d626b477b6f1180901cfd0f7fbddd295d55
Seweryn91 <s.talaj@gmail.com>
2019-01-03 14:24:59 +0100

    Create empty methods

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,34 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    private int size;
+    private int[] data;
+    private int capacity;
+
+    public DynamicIntArray(){
+
+    }
+
+    public DynamicIntArray(int size){
+
+    }
+
+    public void createArray(int size){
+
+    }
+
+    public void add(int index){
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void insert(int index){
+    }
+
 }
