bdcb4d5607b8e1de7050e048f45356c4cfe5aa8b
Seweryn91 <s.talaj@gmail.com>
2019-01-08 15:24:56 +0100

    Implement constructors

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,16 +3,20 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
-    private int size;
-    private int[] data;
-    private int capacity;
+    private int size; //number of elements
+    private int[] data; //array for storing elements
+
 
     public DynamicIntArray(){
-
+        data = new int[1];
+        size = 0;
     }
 
     public DynamicIntArray(int size){
-
+        if (capacity < 1){
+            capacity = 1;}
+        data = new int[capacity];
+        size = 0;
     }
 
     public void createArray(int size){
